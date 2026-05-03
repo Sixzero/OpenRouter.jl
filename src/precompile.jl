9e00e5d0016eb6a29ebb65bd3ab4f7375b97ab82
SixZero <havliktomi@hotmail.com>
2026-05-03 22:14:06 +0200

    Skip precompile workload when echo server port is in use

--- src/precompile.jl
+++ src/precompile.jl
@@ -4,10 +4,14 @@ using PrecompileTools
 
 @setup_workload begin
     @compile_workload begin
-        with_echo_server() do
-            for p in ECHO_PROVIDERS
-                aigen(; prompt="hi", model="$p:test")
+        try
+            with_echo_server() do
+                for p in ECHO_PROVIDERS
+                    aigen(; prompt="hi", model="$p:test")
+                end
             end
+        catch e
+            e isa Base.IOError && contains(e.msg, "address already in use") && @debug "Skipping precompilation: echo server port in use"
         end
     end
 end
